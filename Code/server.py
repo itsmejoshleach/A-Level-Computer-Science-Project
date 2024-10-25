8fe15e811489bd2556d5a2bc29156c3aea05afc6
Cryptoid Coder <josh@coding.thecryptoid.co.uk>
2024-10-25 11:21:44 +0100

    Updated To Port 8080 to work better on linux systems (Sudo not required)

--- Code/server.py
+++ Code/server.py
@@ -141,5 +141,5 @@ def hello_world():
     ) # render html template with all variables passed through
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 80))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
